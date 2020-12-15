05a2940f10593fa108a8f953709724cbfdbf203c
Rii <58388569+Rii0109@users.noreply.github.com>
2020-12-15 13:42:18 +0800

    Update send_message.js
    更改成繁體中文

--- Chinese/actions/Chinese/send_message.js
+++ Chinese/actions/Chinese/send_message.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "发送消息",
+name: "發送消息",
 
 //---------------------------------------------------------------------
 // Action Section
