e3fa7cbb64a11cb13803c997e4a045d8a00b06b1
Rii <58388569+Rii0109@users.noreply.github.com>
2020-12-15 13:42:48 +0800

    Update send_tts_message.js
    更改成繁體中文

--- Chinese/actions/Chinese/send_tts_message.js
+++ Chinese/actions/Chinese/send_tts_message.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "发送TTS消息",
+name: "發送TTS消息",
 
 //---------------------------------------------------------------------
 // Action Section
