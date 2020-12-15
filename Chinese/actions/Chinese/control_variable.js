764cc69c483e3aadd1810ed66af2732d6a1bd8f3
Rii <58388569+Rii0109@users.noreply.github.com>
2020-12-15 12:57:32 +0800

    Update control_variable.js

--- Chinese/actions/Chinese/control_variable.js
+++ Chinese/actions/Chinese/control_variable.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "控制变量",
+name: "控制變量",
 
 //---------------------------------------------------------------------
 // Action Section
