d4e19cf27ec9290595efec789a29204a16a463a6
Rii <58388569+Rii0109@users.noreply.github.com>
2020-12-15 11:50:42 +0800

    更改成中文繁體
    更改成中文繁體

--- Chinese/actions/Chinese/add_reaction.js
+++ Chinese/actions/Chinese/add_reaction.js
@@ -6,7 +6,7 @@ module.exports = {
 // This is the name of the action displayed in the editor.
 //---------------------------------------------------------------------
 
-name: "添加反应",
+name: "添加反應",
 
 //---------------------------------------------------------------------
 // Action Section
